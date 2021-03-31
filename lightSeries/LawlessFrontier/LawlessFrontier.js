6a04b6cce8fc604e7d002c8e52e4cf79c91f001e
Anastacia-HS <kwon7788966@naver.com>
2021-03-31 13:57:29 +0900

    loreSeries slide bar conform

--- lightSeries/LawlessFrontier/LawlessFrontier.js
+++ lightSeries/LawlessFrontier/LawlessFrontier.js
@@ -1,23 +1,4 @@
-$(document).ready(function(){
-    $('.menu_btn>a').on('click', function(){
-        $('.menu_bg').show(); 
-        $('.sidebar_menu').show().animate({
-            right:0
-        });  
-    });
-    
-    $('.close_btn>a').on('click', function(){
-        $('.menu_bg').hide(); 
-        $('.sidebar_menu').animate({
-            right: '-' + 50 + '%'
-        }, function() {
-            $('.sidebar_menu').hide(); 
-
-            }); 
-        });
-    });
-    
-    $(document).ready(function() {
+$(document).ready(function() {
     var menu_offset = $('.nav').offset();
 
     $(window).scroll(function() {
@@ -28,3 +9,23 @@ $(document).ready(function(){
         }
     })
 })
+
+$(document).ready(function(){
+    $('.menu_btn>a').on('click', function(){
+        $('.menu_bg').show(); 
+        $('.sidebar_menu').show().animate({
+            right:0
+        });  
+        $('body').css("overflow", "hidden");
+    });
+    
+    $('.close_btn>a').on('click', function(){
+        $('.menu_bg').hide(); 
+        $('.sidebar_menu').animate({
+            right: '-' + 50 + '%'
+        }, function() {
+            $('.sidebar_menu').hide(); 
+                $('body').css("overflow", "scroll");
+            }); 
+        });
+    });
