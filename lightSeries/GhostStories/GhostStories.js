9dbc20c5cef2d3edb67833800ce01f62ff4c5405
Hyesoo Kwon <kwon7788966@gmail.com>
2021-04-01 13:20:35 +0900

    GhostStories js create

---
+++ lightSeries/GhostStories/GhostStories.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    var menu_offset = $('.nav').offset();
+
+    $(window).scroll(function() {
+        if($(document).scrollTop() > menu_offset.top) {
+            $('.nav').addClass('nav-fixed')
+        }else {
+            $('.nav').removeClass('nav-fixed')
+        }
+    })
+})
+
+$(document).ready(function(){
+    $('.menu_btn>a').on('click', function(){
+        $('.menu_bg').show(); 
+        $('.sidebar_menu').show().animate({
+            right:0
+        });  
+        $('body').css("overflow", "hidden");
+    });
+    
+    $('.close_btn>a').on('click', function(){
+        $('.menu_bg').hide(); 
+        $('.sidebar_menu').animate({
+            right: '-' + 50 + '%'
+        }, function() {
+            $('.sidebar_menu').hide(); 
+                $('body').css("overflow", "scroll");
+            }); 
+        });
+    });
